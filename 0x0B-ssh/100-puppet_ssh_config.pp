3ad332121336a31b9d2f1323d767e974987a4c5f
fae713 <favourmichael47@gmail.com>
2024-01-21 09:29:57 -0800

    advanced task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# A puppet script that automates changes to our configuration file
+
+$file_content = file('/etc/ssh/ssh_config')
+$config = "${file_content}\
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+"
+file {  'school':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  content => $config
+}
