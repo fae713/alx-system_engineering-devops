6b0519ef7e6faef562fcf80638089c800dc2afec
fae713 <favourmichael47@gmail.com>
2024-03-05 13:19:07 -0800

    hot 10 tittles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,3 +1,32 @@
 #!/usr/bin/python3
 """
- a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+A function that queries the Reddit API and prints
+the titles of the first 10 hot posts listed for a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Returns the titles of the top 10 hot posts"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json?limit=10"
+    headers = {
+            "User-Agent": "Mozilla/5.0"
+            }
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+            titles = [post['data']['title'] for post in posts]
+            for title in titles:
+                print(title)
+        else:
+            print(None)
+    except request.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        print(None)
+
+
+if __name__ == "__main__":
+    top_ten(agrv[1])
